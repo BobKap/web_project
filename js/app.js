db3d8d38ad8398584531658057b551c6c3e09a4f
unknown <bobKaplanjr47@twc.com>
2018-02-21 14:01:23 -0500

    Added proper positioning of nav pop up.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("#navPopUp").dialog( {
+    $("#navPopUp").dialog({
+        position: { my: "center", at: "top+25%", of: window },
         autoOpen: false,
         draggable: false,
         resizable: false,
         modal: true,
-        // position: [400, 400]
         hide: 'explode'
-    }); // end PopUp dialog
+    }); // end PopUp dialog definition
 
-    $('.nav-link, .btn-search').click(function(evt) {
+    $('.nav-link, .btn-search').click(function (evt) {
         evt.preventDefault();
         $('#navPopUp').dialog('open');
-    }); //end click
+    }); //end click event
 
 });  // end ready
 
