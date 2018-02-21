a4c71b9a381124d45473fb1b74e050c6fa994f6d
unknown <bobKaplanjr47@twc.com>
2018-02-21 12:47:38 -0500

    Working on nav Pop Up

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
-/* need ready function */
+$(document).ready(function() {
+
+    $("#navPopUp").dialog( {
+        autoOpen: false,
+        draggable: false,
+        resizable: false,
+        modal: true,
+        // position: [400, 400]
+        hide: 'explode'
+    }); // end PopUp dialog
+
+    $('.nav-item').click(function(evt) {
+        evt.preventDefault();
+        $('#navPopUp').dialog('open');
+    }); //end click
+
+});  // end ready
 
